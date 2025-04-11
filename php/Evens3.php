35b882431d24fbd12c64f83f43ce98e016e17987
Yangchanthorony <tonyja2460@gmail.com>
2025-04-11 19:47:26 +0700

    push

--- php/Evens3.php
+++ php/Evens3.php
@@ -38,11 +38,11 @@ include("../components/header.php");
 
         <!-- Social Media Share Buttons -->
         <div class="social-share mt-4">
-            <p class="fw-bold" id="share-event-title">Share this Event</p>
-        <a href="https://web.facebook.com/ppiu.edu.kh.ppiucam"> <button class="btn btn-primary"><i class="fab fa-facebook-f"></i></button></a>
-        <button class="btn btn-dark"><i class="fab fa-x-twitter"></i></button>
-        <button class="btn btn-info text-white"><i class="fab fa-linkedin-in"></i></button>
-        <button class="btn btn-success"><i class="fab fa-whatsapp"></i></button>
+        <p class="fw-bold" id="share-event-titleo">Share this Event</p>
+       <a href="https://web.facebook.com/ppiu.edu.kh.ppiucam"> <button class="btn btn-primary"><i class="fab fa-facebook-f"></i></button></a>
+        <a href="https://x.com/ppiuoffical"><button class="btn btn-dark"><i class="fab fa-x-twitter"></i></button></a>
+        <a href="https://www.linkedin.com/company/phnompenh-international-university/"><button class="btn btn-info text-white"><i class="fab fa-linkedin-in"></i></button></a>
+        <a  href="https://www.youtube.com/channel/UCtQK_K719j7z3urWlNyeQjA?view_as=subscriber"><button class="btn btn-danger"><i class="fab fa-youtube"></i></button></a>
     </div>
 </div>
 <?php 
